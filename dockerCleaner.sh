6a441ad3259aa81bd71ffd176ada7511b9d3a1f7
arnheidur13 <arnheidur13@ru.is>
2016-12-08 18:18:25 +0000

    Fixes for migratedb and production

--- dockerCleaner.sh
+++ dockerCleaner.sh
@@ -1,9 +1,11 @@
-echo "Kil running docker containers"
-sudo docker kill $(sudo docker ps -q)
+#echo "Kil running docker containers"
+#sudo docker kill $(sudo docker ps -a -q)
+
+echo "Stop all running containers"
+sudo docker kill $(docker ps -a -q)
 
 echo "Removing docker containers"
-sudo docker rm $(sudo docker --all -q)
+sudo docker rm $(sudo docker ps -a -q)
 
-echo "Deleting docker images"
+#echo "Deleting docker images"
 sudo docker rmi $(sudo docker images -q)
-
